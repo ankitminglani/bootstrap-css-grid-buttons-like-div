3211fc7dececeacbc26db6b780e244db817d5aea
Ankit Minglani <ankit.minglani@gmail.com>
2019-04-20 00:32:49 +0530

    updates to POC for responsive design for bootstrap 4 grid with clickable divs with centred images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { MessageService } from 'primeng/components/common/messageservice';
 
 @Component({
-  selector: 'my-app',
+  selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
-  name = 'Angular';
+export class AppComponent {
+  title = 'BootstrapGrid';
+
+  constructor(private messageService: MessageService) {}
+
+  buttonDivClicked(value:string):void{
+    console.log(value);
+    this.messageService.add({severity:'info', summary:'New Message', detail:value+' clicked!'});
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,21 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import {ToastModule} from 'primeng/toast';
+import { MessageService } from 'primeng/components/common/messageservice';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    ToastModule
+  ],
+  providers: [MessageService],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
